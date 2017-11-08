fc6a52aec89076792c0a4f6db73f2c34f8b789e2
Anna Fasolyak <AnnaFasolyak@Annas-MacBook-Pro-2.local>
2017-11-07 21:25:02 -0500

    Saving Changes

--- javascript/main.js
+++ javascript/main.js
@@ -9,3 +9,43 @@
 // Both parameters must be in quotes.
 
 // toggleClass('.myClass', 'class-to-toggle')
+
+"use strict";
+
+//Menu
+
+function toggleMenu() {
+  var menuBox = document.getElementById('links');    
+  if(links.style.display == "none" || links.style.display == "") {
+    menu.innerHTML = "CLOSE";
+    links.style.display = "block";
+  }
+  else { 
+    menu.innerHTML = "MENU";
+    links.style.display = "none";
+  }
+}
+
+//Image Slideshow
+
+var slide_index = 1;  
+        displaySlides(slide_index);  
+  
+        function nextSlide(n) {  
+            displaySlides(slide_index += n);  
+        }  
+  
+        function currentSlide(n) {  
+            displaySlides(slide_index = n);  
+        }  
+  
+        function displaySlides(n) {  
+            var i;  
+            var slides = document.getElementsByClassName("showSlide");  
+            if (n > slides.length) { slide_index = 1 }  
+            if (n < 1) { slide_index = slides.length }  
+            for (i = 0; i < slides.length; i++) {  
+                slides[i].style.display = "none";  
+            }  
+            slides[slide_index - 1].style.display = "block";  
+        }
